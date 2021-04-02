f4ce8546dd933040064f4278dcbdba96612dc555
horor203 <81828805+horor203@users.noreply.github.com>
2021-04-02 23:05:19 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,3 @@
+function ok() {
+    document.getElementsByTagName('body')[0].innerHTML = "<div class='video-container' width='500px'><video src='media/oops.mp4' autoplay loop style='width: 100%'></video></div> "
+}
